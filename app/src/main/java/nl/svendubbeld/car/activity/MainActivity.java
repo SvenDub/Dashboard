fce8b6cbebcf61b0b27a4b8b65a7995e1068b304
Sven Dubbeld <sven.dubbeld1@gmail.com>
2016-02-12 20:25:00 +0100

    Show date and time

--- app/src/main/java/nl/svendubbeld/car/activity/MainActivity.java
+++ app/src/main/java/nl/svendubbeld/car/activity/MainActivity.java
@@ -1,16 +1,29 @@
 package nl.svendubbeld.car.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextClock;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import nl.svendubbeld.car.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextClock mDateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        mDateView = (TextClock) findViewById(R.id.date);
+        DateFormat shortDateFormat = android.text.format.DateFormat.getMediumDateFormat(getApplicationContext());
+        if (shortDateFormat instanceof SimpleDateFormat) {
+            mDateView.setFormat24Hour(((SimpleDateFormat) shortDateFormat).toPattern());
+            mDateView.setFormat12Hour(((SimpleDateFormat) shortDateFormat).toPattern());
+        }
     }
 }
